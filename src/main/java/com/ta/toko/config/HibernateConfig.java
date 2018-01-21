678d4e43de7aa06da522004b9a03565d3d124fe2
F Sanaky <sybers_echo@yahoo.co.id>
2018-01-21 16:14:28 +0700

    change to h2 and fix header url

--- src/main/java/com/ta/toko/config/HibernateConfig.java
+++ src/main/java/com/ta/toko/config/HibernateConfig.java
@@ -4,12 +4,12 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -32,31 +32,14 @@ public class HibernateConfig {
 	private Properties additionalProperties() {
 		Properties prop = new Properties();
 		prop.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-		// prop.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-		prop.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServer2012Dialect");
+		prop.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		// prop.setProperty("hibernate.dialect",
+		// "org.hibernate.dialect.SQLServer2012Dialect");
 		prop.setProperty("hibernate.show_sql", "true");
 		prop.setProperty("hibernate.format_sql", "false");
 		prop.setProperty("hibernate.enable_lazy_load_no_trans", "true");
 		return prop;
 	}
-
-	// @Bean
-	// public DataSource basicDataSource() {
-	// // EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-	// // EmbeddedDatabase dataSource =
-	// //
-	// builder.setType(EmbeddedDatabaseType.H2).addScript("db/sql/insert-data.sql").build();
-	// BasicDataSource dataSource = new BasicDataSource();
-	// dataSource.setUsername("sa");
-	// dataSource.setPassword("sa");
-	// dataSource.setValidationQuery("SELECT 1;");
-	// dataSource.setUrl("jdbc:h2:mem:store");
-	// dataSource.setDriverClassName("org.h2.Driver");
-	// dataSource.setInitialSize(5);
-	// // dataSource.set
-	// // dataSource.setmax
-	// return dataSource;
-	// }
 
 	@Bean
 	public DataSource basicDataSource() {
@@ -64,17 +47,36 @@ public class HibernateConfig {
 		// EmbeddedDatabase dataSource =
 		//
 		// builder.setType(EmbeddedDatabaseType.H2).addScript("db/sql/insert-data.sql").build();
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUsername("ta");
-		dataSource.setPassword("Admin@3456");// Admin@3456
-		// dataSource.setValidationQuery("SELECT 1;");
-		dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=kupalatu_store");
-		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		// dataSource.setInitialSize(5);
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setUsername("sa");
+		dataSource.setPassword("sa");
+		dataSource.setValidationQuery("SELECT 1;");
+		dataSource.setUrl("jdbc:h2:mem:store");
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setInitialSize(5);
 		// dataSource.set
 		// dataSource.setmax
 		return dataSource;
 	}
+
+	// @Bean
+	// public DataSource basicDataSource() {
+	// // EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+	// // EmbeddedDatabase dataSource =
+	// //
+	// //
+	// builder.setType(EmbeddedDatabaseType.H2).addScript("db/sql/insert-data.sql").build();
+	// DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	// dataSource.setUsername("ta");
+	// dataSource.setPassword("Admin@3456");// Admin@3456
+	// // dataSource.setValidationQuery("SELECT 1;");
+	// dataSource.setUrl("jdbc:sqlserver://localhost:1433;databaseName=kupalatu_store");
+	// dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	// // dataSource.setInitialSize(5);
+	// // dataSource.set
+	// // dataSource.setmax
+	// return dataSource;
+	// }
 
 	@Bean
 	public HibernateTransactionManager transactionManager(SessionFactory s) {
--- src/main/webapp/WEB-INF/views/common/header.jsp
+++ src/main/webapp/WEB-INF/views/common/header.jsp
@@ -9,7 +9,7 @@
 <!-- Main Header -->
 <header class="main-header">
 	<!-- Logo -->
-	<a href="index2.html" class="logo"> <!-- mini logo for sidebar mini 50x50 pixels -->
+	<a href="#" class="logo"> <!-- mini logo for sidebar mini 50x50 pixels -->
 		<span class="logo-mini"><b>K</b> St</span> <!-- logo for regular state and mobile devices -->
 		<span class="logo-lg"><b>K</b>Store</span>
 	</a>
